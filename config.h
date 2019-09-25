7a8641f85e0199948db7ffd5ae9a18e7b459580f
Ahrcantos <ahrcantos99@gmail.com>
2019-09-25 15:09:07 +0200

    changed font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
-static char *font2[] = { "Inconsolata for Powerline:pixelsize=11:antialias=true:autohint=true" };
+static char *font = "Iosevka:pixelsize=11:antialias=true:autohint=true";
+static char *font2[] = { "Iosevka:pixelsize=11:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
